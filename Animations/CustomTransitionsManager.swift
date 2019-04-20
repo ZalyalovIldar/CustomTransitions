b62e27184a5ec40efb4c74eb6cc84de742693421
Guzel Sharafutdinova <guzelsharafutdinova@icloud.com>
2019-04-20 17:48:09 +0300

    second

--- Animations/CustomTransitionsManager.swift
+++ Animations/CustomTransitionsManager.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import UIKit
+
+class CustomTransitionsManager: NSObject, UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate {
+    
+    var isPresented = false
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.8
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        guard let toView = transitionContext.view(forKey: .to),
+            let fromView = transitionContext.view(forKey: .from),
+            let toViewSnapShot = toView.snapshotView(afterScreenUpdates: true)
+            else {
+                
+                transitionContext.completeTransition(false)
+                return
+        }
+    }
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        isPresented = true
+        
+        return self
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        isPresented = false
+        
+        return self
+    }
+}
